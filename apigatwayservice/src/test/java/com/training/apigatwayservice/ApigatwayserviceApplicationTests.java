47adb8cdb562629f6d0ce5a86a0e8275e7e0e5d5
HarshaVardhan <Harshavardhan.Athaluri@ge.com>
2021-04-23 09:37:20 +0530

    apigateway

---
+++ apigatwayservice/src/test/java/com/training/apigatwayservice/ApigatwayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.apigatwayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatwayserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
